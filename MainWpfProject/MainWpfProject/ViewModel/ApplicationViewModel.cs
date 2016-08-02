499f11575b3e24c35409e355073cec03cca37cfc
mrlor <mrlor@DESKTOP-K4473GC>
2016-08-02 20:54:19 +0100

    navigation Service Created

---
+++ MainWpfProject/MainWpfProject/ViewModel/ApplicationViewModel.cs
@@ -0,0 +1,43 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MainWpfProject.ViewModel
+{
+    public class ApplicationViewModel : ObservableObject
+    {
+        #region Fields
+        private ICommand _changePageCommand;
+        private IPageViewModel _currentPageViewModel;
+        private List<IPageViewModel> _pageViewModels;
+        #endregion
+        public ApplicationViewModel()
+        {
+            //PageViewModels.Add(MainViewModel);
+        }
+
+    public List<IPageViewModel> PageViewModels
+        {
+            get
+            {
+                if (_pageViewModels == null)
+                    _pageViewModels = new List<IPageViewModel>();
+
+                return _pageViewModels;
+            }
+        }
+
+        public IPageViewModel CurrentPageViewModel
+        {
+            get
+            {
+                return _currentPageViewModel;
+            }
+        }
+
+    }
+}
